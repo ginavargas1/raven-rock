56e3f1e7f1451c64f8a8952cfb3c377943fdcc6c
Edward Arana <Edwardaranadlt@gmail.com>
2021-05-25 11:41:56 -0700

    changed the placeholder to a conatiner instead

--- raven-rock/src/pages/Landing.js
+++ raven-rock/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from 'semantic-ui-react'
 import { Placeholder } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 
 
 function Landing(){
@@ -13,14 +14,29 @@ function Landing(){
       <Header as='h1'>Let's get this bread</Header>
 
       {/* Description about our website Using Lorem as placeholder */}
-      <Placeholder>
-        <Placeholder.Line />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec nam aliquam sem et tortor consequat id porta. In ornare quam viverra orci sagittis. Ac tincidunt vitae semper quis. Dictum non consectetur a erat nam at lectus. Sapien nec sagittis aliquam malesuada bibendum arcu. Amet justo donec enim diam vulputate. Arcu cursus euismod quis viverra nibh cras pulvinar. Habitant morbi tristique senectus et netus et malesuada. Elementum integer enim neque volutpat ac tincidunt vitae semper quis. Vitae et leo duis ut diam quam nulla porttitor massa. Morbi non arcu risus quis varius quam. Morbi tempus iaculis urna id volutpat lacus. Pellentesque sit amet porttitor eget. Pharetra sit amet aliquam id diam maecenas ultricies. A diam maecenas sed enim. Mi bibendum neque egestas congue quisque. Sagittis nisl rhoncus mattis rhoncus urna neque. Morbi blandit cursus risus at ultrices mi tempus.
-        <Placeholder.Line /> <br></br>
-        <Placeholder.Line />Nulla facilisi nullam vehicula ipsum a. A diam maecenas sed enim ut sem viverra. Morbi tempus iaculis urna id volutpat lacus laoreet. Purus ut faucibus pulvinar elementum integer. Lacus suspendisse faucibus interdum posuere. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Lacinia at quis risus sed. Vel quam elementum pulvinar etiam non. Eget nunc scelerisque viverra mauris in aliquam sem. Eget nunc lobortis mattis aliquam faucibus. Odio ut enim blandit volutpat maecenas volutpat blandit. Id velit ut tortor pretium viverra suspendisse potenti. Condimentum id venenatis a condimentum. In fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Auctor elit sed vulputate mi sit. Lacus viverra vitae congue eu. Auctor eu augue ut lectus arcu bibendum. Vitae turpis massa sed elementum.
-        <Placeholder.Line /> <br></br>
-        <Placeholder.Line />Gravida dictum fusce ut placerat orci. Tellus in metus vulputate eu scelerisque felis imperdiet. Viverra mauris in aliquam sem. Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Turpis massa sed elementum tempus egestas sed. Sed cras ornare arcu dui vivamus. Sit amet massa vitae tortor condimentum lacinia quis. Blandit turpis cursus in hac habitasse platea dictumst. Lectus proin nibh nisl condimentum id venenatis a. Nullam non nisi est sit. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Eu feugiat pretium nibh ipsum. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Blandit aliquam etiam erat velit scelerisque in dictum non consectetur. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Nunc sed velit dignissim sodales ut eu sem.
-        <Placeholder.Line /> <br></br>
-      </Placeholder>
+
+      <Container fluid>
+
+      <Header as='h2'>About Raven Rock</Header>
+      <p>
+        Domestic dogs inherited complex behaviors, such as bite inhibition, from
+        their wolf ancestors, which would have been pack hunters with complex
+        body language. These sophisticated forms of social cognition and
+        communication may account for their trainability, playfulness, and
+        ability to fit into human households and social situations, and these
+        attributes have given dogs a relationship with humans that has enabled
+        them to become one of the most successful species on the planet today.
+      </p>
+      <p>
+        The dogs' value to early human hunter-gatherers led to them quickly
+        becoming ubiquitous across world cultures. Dogs perform many roles for
+        people, such as hunting, herding, pulling loads, protection, assisting
+        police and military, companionship, and, more recently, aiding
+        handicapped individuals. This impact on human society has given them the
+        nickname "man's best friend" in the Western world. In some cultures,
+        however, dogs are also a source of meat.
+      </p>
+    </Container>
       
 
 
