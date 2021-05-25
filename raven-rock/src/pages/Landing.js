fbb1f3b517995a0945afead422a50827bb85f7ba
Edward Arana <Edwardaranadlt@gmail.com>
2021-05-25 11:57:10 -0700

    working on the launch page, also added three buttons with modals

--- raven-rock/src/pages/Landing.js
+++ raven-rock/src/pages/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from 'semantic-ui-react'
-import { Placeholder } from 'semantic-ui-react'
 import { Container } from 'semantic-ui-react'
-
+import { Button, Image, Modal } from 'semantic-ui-react'
 
 function Landing(){
+
+ 
+  const [open, setOpen] = React.useState(false)
 
   
   
@@ -14,7 +16,6 @@ function Landing(){
       <Header as='h1'>Let's get this bread</Header>
 
       {/* Description about our website Using Lorem as placeholder */}
-
       <Container fluid>
 
       <Header as='h2'>About Raven Rock</Header>
@@ -37,11 +38,108 @@ function Landing(){
         however, dogs are also a source of meat.
       </p>
     </Container>
+
+    {/* Section Below will be used for our modals and templates */}
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button>Template Number One</Button>}
+    >
+      <Modal.Header>Select a Photo</Modal.Header>
+      <Modal.Content image>
+        <Image size='medium' src='/images/avatar/large/rachel.png' wrapped />
+        <Modal.Description>
+          <Header>Default Profile Image</Header>
+          <p>
+            We've found the following gravatar image associated with your e-mail
+            address.
+          </p>
+          <p>Is it okay to use this photo?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => setOpen(false)}>
+          Nope
+        </Button>
+        <Button
+          content="Yep, that's me"
+          labelPosition='right'
+          icon='checkmark'
+          onClick={() => setOpen(false)}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+    {/* End Of Modal One */}
+
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button>Template Number Two</Button>}
+    >
+      <Modal.Header>Select a Photo</Modal.Header>
+      <Modal.Content image>
+        <Image size='medium' src='/images/avatar/large/rachel.png' wrapped />
+        <Modal.Description>
+          <Header>Default Profile Image</Header>
+          <p>
+            We've found the following gravatar image associated with your e-mail
+            address.
+          </p>
+          <p>Is it okay to use this photo?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => setOpen(false)}>
+          Nope
+        </Button>
+        <Button
+          content="Yep, that's me"
+          labelPosition='right'
+          icon='checkmark'
+          onClick={() => setOpen(false)}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+    {/* End Of Modal Two */}
+
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button>Template Number Three</Button>}
+    >
+      <Modal.Header>Select a Photo</Modal.Header>
+      <Modal.Content image>
+        <Image size='medium' src='/images/avatar/large/rachel.png' wrapped />
+        <Modal.Description>
+          <Header>Default Profile Image</Header>
+          <p>
+            We've found the following gravatar image associated with your e-mail
+            address.
+          </p>
+          <p>Is it okay to use this photo?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => setOpen(false)}>
+          Nope
+        </Button>
+        <Button
+          content="Yep, that's me"
+          labelPosition='right'
+          icon='checkmark'
+          onClick={() => setOpen(false)}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+
+    {/* End Of Modal Three */}
       
-
-
-
-
 
     </div>
   )
